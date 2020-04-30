4d1b344494dee14857b1a8046f6a2333a68f94e1
lsavoie <twosavoie@gmail.com>
2020-04-30 18:13:42 -0500

    functions

--- beginner-javascript-master/playground/custom-functions/cf.js
+++ beginner-javascript-master/playground/custom-functions/cf.js
@@ -1,16 +1,20 @@
-// Function Definition
+// Function Definition (with parameters (which are variables in a function) will be passed arguments)
+// Parameters are Placeholders (or Possibles) and Arguments are Actual values (mnemonic device)
+// Functions are "First Class Citizens" meaning they can be passed into other functions, variables... treated as
 function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
-  // this is the function body
+  // this is the function body within the function block
   console.log('Running Calculate Bill!!');
   const total = billAmount + billAmount * taxRate + billAmount * tipRate;
-  return total;
+  return total; // Need to return (keyword) to store the result of calculation in order to access it
 }
 
-// Function Call. Or **Run**
+// Function Call. Or **Run** of the function
+// calculateBill();
 const wesTotal = 500;
 const wesTaxRate = 0.3;
-// const myTotal = calculateBill(wesTotal, wesTaxRate);
+// const myTotal = calculateBill(wesTotal, wesTaxRate); //Passing variables to arguments //Capturing the returned value of the function?
 
+// Looks first to local scope for defined variable. If not then to higher or agument passed
 // Function Definition
 function sayHiTo(firstName) {
   return `Hello ${firstName}`;
@@ -19,13 +23,20 @@ function sayHiTo(firstName) {
 // const greeting = sayHiTo('Wes');
 // console.log(greeting);
 
+// can also pass expressions and functions as arguments
+
+// can also pass functions as arguments
 function doctorize(name) {
   return `Dr. ${name}`;
 }
 
+// Set a default value in function definition (to avoid breaking code). Can be an empty string
 function yell(name = 'Silly Goose') {
   return `HEY ${name.toUpperCase()}`;
 }
 
+// Pass undefined to a function so that it uses the default value
 const myBill4 = calculateBill(100, undefined, 0.2);
 console.log(myBill4);
+
+// Can pass mutiple things to the parameters of a function. Default value (use undefined when calling a function to access it), Variables scoped locally or globally (including empty string) arguments, returns from functions, functions, expressions
--- beginner-javascript-master/playground/custom-functions/ways-to-make-a-function.js
+++ beginner-javascript-master/playground/custom-functions/ways-to-make-a-function.js
@@ -1,18 +1,35 @@
+// Declare a function with a name
 // function doctorize(firstName) {
 //   return `Dr. ${firstName}`;
 // }
 
-// Anon Function
+// Anon Function (without a name) Not valid JS. Can be used case as a callback or IFFE(?)
 // function (firstName) {
 //   return `Dr. ${firstName}`;
 // }
 
-// Function Expression
+// Function Expression store a function as a value in a variable. An anonymous function in a variable. Will not be hoisted.
 // const doctorize = function(firstName) {
 //   return `Dr. ${firstName}`;
 // };
 
 /* eslint-disable */
+// Arrow functions. Don't have their own scope with "this" keyword. Are anonymous functions
+// This:
+// function inchToCM(inches) {
+//    const cm = inches * 2.54;
+//    return cm;
+//}
+// Could become this:
+//  function inchToCM(inches) {
+//     return inches * 2.54;
+//};
+// Refactored to an anonymous function
+//  const incheToCM = functions(inches) {
+//  return inches * 2.54;
+//};
+// Becomes this: remove "function", add fat arrow, implicit return (all on one line, remove curly, delete "return", remove extra ";")
+// If only one parameter can get rid of parentheses
 const inchToCM = inches => inches * 2.54;
 
 // function add(a, b = 3) {
@@ -31,47 +48,54 @@ const inchToCM = inches => inches * 2.54;
 //   }
 //   return baby;
 // }
-
+// Implicitly return a object - surround what you are returning with paretheses
+// Don't have to write an arrow function if it leads to less readable code
 const makeABaby = (first, last) => ({ name: `${first} ${last}`, age: 0 });
 
 
 // IIFE
-// Immediately Invoked Function Expression
-
+// Immediately Invoked Function Expression // Or Immediately Run Function Expression (rather than called later?)
+// Wrap in parentheses (parentheses always run first)
 (function(age) {
   return `You are cool and age ${age}`;
 })(10);
 
 // Methods!!!
+// A function that lives inside of an object (console.log: log is a function, console is the object. So this is a method)
 const wes = {
   name: 'Westopher Bos',
-  // Method!
+  // Method! this is it :) (btw, the name of this function is inferred)
   sayHi: function() {
     console.log(`Hey ${this.name}`);
     return 'Hey Wes';
   },
-  // Short hand Method
+  // Short hand Method (common)
   yellHi() {
     console.log('HEY WESSSSS');
   },
-  // Arrow function
+  // Arrow function (less common)
   wisperHi: () => {
     console.log('hii wesss im a mouse');
   }
 }
 
 // Callback Functions
+// Something will happen when something is done. When someone clicks something do this or when an amount of time has passed, do this.
+// A function is passed to a function to be called later
 // Click Callback
 const button = document.querySelector('.clickMe');
+// button.addEventListener('click', wes.yellHI); // When click happens "callback" to the yellHI function
 
+// Can be called outside of the 
 function handleClick() {
   console.log('Great Clicking!!');
 }
 
+// Passing in an anonymous function
 button.addEventListener('click', function() {
   console.log('NIce Job!!!');
 });
-
+// This is refactored three... times - see video
 // Timer Callback
 setTimeout(() => {
   console.log('DONE! Time to eat!');
