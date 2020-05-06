59713d09003227a5d047320efbe1f8d2f887b154
lsavoie <twosavoie@gmail.com>
2020-05-06 18:29:28 -0500

    event listeners

--- beginner-javascript-master/exercises/29 - Events/events.js
+++ beginner-javascript-master/exercises/29 - Events/events.js
@@ -1,38 +1,60 @@
+// to listen, must get the element first
 const butts = document.querySelector('.butts');
 const coolButton = document.querySelector('.cool');
 
+//
 function handleClick() {
   console.log('🐛 IT GOT CLICKED!!!');
 }
 
 const hooray = () => console.log('HOORAY!');
 
+// then add an event listener - usually with two arguments, what is the event & callback function
+// callback function is a regular function - the browser will call (or run) the function when the button is clicked (anonymous function)
+// can also pass in a function defined elsewhere for instance a handleClick or hooray
+// Go get something, listen for something, do something
+// Don't need the parentheses after the call back functions that are previously defined because the browser is calling them, not us
 butts.addEventListener('click', function() {
   console.log('Im an anon!');
 });
 coolButton.addEventListener('click', hooray);
 
 butts.removeEventListener('click', handleClick);
+// benefit to having the function outside of the event listener: can use the same callback function for other event listener functions including removeEventListner. It's DRY and it's necessary to use a named function or an arrow function (using the variable)
+// handleClick is BOUND to butts. removing it UNBINDS
 
-// Listen on multiple items
+// Listen on multiple items: querySelectorAll
 const buyButtons = document.querySelectorAll('button.buy');
 
+// Can call parameter anything because the browser is calling it
+// event must be the first parameter
 function handleBuyButtonClick(event) {
   console.log('You clicked a button!');
-  const button = event.target;
+  const button = event.target; // shows which button the user has clicked on and anything associated with can be specified
+  // console.lot(event.target.dataset)
+  // console.lot(event.target.dataset.price)
   // console.log(button.textContent);
-  // console.log(parseFloat(event.target.dataset.price));
-  console.log(event.target);
-  console.log(event.currentTarget);
+  // console.log(parseFloat(event.target.dataset.price)); // change the string price to number
+  console.log(event.target); // what got clicked
+  console.log(event.currentTarget); // what fired the event listener
   console.log(event.target === event.currentTarget);
   // Stop this event from bubbling up
-  // event.stopPropagation();
+  // event.stopPropagation(); // stop bubbling up from where the use clicked through the DOM tree
 }
 
+// Will run a function for each of our nodes (passed as a parameter)
 buyButtons.forEach(function(buyButton) {
   buyButton.addEventListener('click', handleBuyButtonClick);
 });
+// the same but wouldn't be able to unbind because, since it is an arrow function, it is an anonymous function
+buyButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    console.log('You Clicked It!');
+  });
+});
 
+// window is the entire browser window
+// Propogation is that a user is clicking on multiple elements/nodes at once
 window.addEventListener(
   'click',
   function(event) {
@@ -42,12 +64,14 @@ window.addEventListener(
     // event.stopPropagation();
     console.log(event.bubbles);
   },
-  { capture: true }
+  { capture: true } // third argument halting capture/bubbling (learn more)
 );
 
 const photoEl = document.querySelector('.photo');
 
+// mouseenter is a specific thing also mousemove...
 photoEl.addEventListener('mouseenter', e => {
   console.log(e.currentTarget);
-  console.log(this);
+  console.log(this); // this is a keyword; this is what is left of the dot. This keyword, using an arrow function, this isn't scoped any longer (say nested functions) so don't maybe use this in this situation
 });
+// the whole point is that multiple things can be listening for clicks and you want to make sure you're only triggering listening for what you want to.
--- beginner-javascript-master/exercises/29 - Events/forms-FINISHED.js
+++ beginner-javascript-master/exercises/29 - Events/forms-FINISHED.js
@@ -1,3 +1,4 @@
+// select the cog in the console if the console log disappears
 const wes = document.querySelector('.wes');
 
 wes.addEventListener('click', function(event) {
@@ -5,19 +6,24 @@ wes.addEventListener('click', function(event) {
     'This website might be malicious!, do you wish to proceed?'
   );
   if (!shouldChangePage) {
-    event.preventDefault();
+    event.preventDefault(); // prevents default action
   }
 });
+// user receives a popup asking if they want to proceed. If they select no then the default action is prevented and they stay on the page
 
-const signupForm = document.querySelector('[name="signup"]');
+const signupForm = document.querySelector('[name="signup"]'); // [] to select based on its name - attribute selector
 
 signupForm.addEventListener('submit', function(event) {
   const name = event.currentTarget.name.value;
   if (name.includes('chad')) {
     alert('Sorry bro');
-    event.preventDefault();
+    event.preventDefault(); // prevents default action
   }
 });
+// console.dir(event.currentTarget) to get properties for inputs
+// because elements have a name
+// console.log(event.currentTarget.name.value);
+// event.currentTarget.querySelector('[name="email"]')
 
 function logEvent(event) {
   console.log(event.type);
@@ -31,7 +37,14 @@ signupForm.name.addEventListener('blur', logEvent);
 // 'keydown'
 // 'focus'
 // 'blur'
+// creates a function to console.log - cool :)
 
+// accessibility
+// buttons are for actions inside an application
+// links are to change the page
+// things that are not keyboard accessible should not have clicks on them (can't tab through)
+// solution is to give the photo... a role="button", tabindex="0", or wrap with a button tag - then can tab through
+// change event listener to add a 'keyup'
 const photo = document.querySelector('.photo');
 
 function handlePhotoClick(event) {
