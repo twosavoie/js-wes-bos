2ead7c757d32bd96c7255f08fd152420bd0a6e42
lsavoie <twosavoie@gmail.com>
2020-05-17 16:15:33 -0500

    tabs

--- beginner-javascript-master/exercises/36 - Tabs/tabs-finished.js
+++ beginner-javascript-master/exercises/36 - Tabs/tabs-finished.js
@@ -1,20 +1,26 @@
+// Grab elements to be acted on by JS
+// Selecting tab & tabpanel means you can have multiple tabs on a site and reuse this code
 const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role="tab"]');
 const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
 
+// Callback functions
 function handleTabClick(event) {
-  // hide all tab panels
+  // write pseudocode to lay out code to write
+  // hide all tab panels; easier to hide them all
+  // tabPanels.forEach(function(panel) {panel.hidden = true;}); becomes an arrow function
   tabPanels.forEach(panel => {
     panel.hidden = true;
   });
   // mark all tabs as unselected
   tabButtons.forEach(tab => {
-    // tab.ariaSelected = false;
+    // tab.ariaSelected = false;  doesn't work - doesn't work for custom or aria attributes
     tab.setAttribute('aria-selected', false);
   });
   // mark the clicked tab as selected
   event.currentTarget.setAttribute('aria-selected', true);
   // find the associated tabPanel and show it!
+  // deconstructoring pulles event.currentTarget.id to {id}
   const { id } = event.currentTarget;
 
   /*
@@ -25,7 +31,7 @@ function handleTabClick(event) {
   */
 
   // METHOD 2 - find in the array of tabPanels
-  console.log(tabPanels);
+  console.log(tabPanels); // shows that it is a nodelist not an array so change variable above (find only works on arrays)
   const tabPanel = tabPanels.find(
     panel => panel.getAttribute('aria-labelledby') === id
   );
